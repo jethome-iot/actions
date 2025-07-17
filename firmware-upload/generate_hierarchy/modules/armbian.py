fa8bcb27edb8e30009efc2e40ba84939601dbb0c
Viacheslav Bocharov <adeep@lexina.in>
2025-07-17 18:30:11 +0300

    add cli type to armbian

--- firmware-upload/generate_hierarchy/modules/armbian.py
+++ firmware-upload/generate_hierarchy/modules/armbian.py
@@ -7,19 +7,36 @@ def generate(subtypes, load_json):
     load_json: функция-загрузчик JSON (должна быть передана из main.py)
     """
     if len(subtypes) < 3:
-        print("Error: For Armbian, need channel, release, branch")
+        print("Error: For Armbian, need at least channel, release, branch")
         sys.exit(2)
-    channel, release, branch = subtypes[:3]
+
+    if len(subtypes) == 4:
+        channel, release, clitype, branch = subtypes[:4]
+    else:
+        channel, release, branch = subtypes[:3]
+        clitype = None
+
     releases = load_json('armbian/releases.json')
     branches = load_json('armbian/branches.json')
     channels = load_json('channels.json')
+    clitypes = load_json('armbian/clitypes.json') if clitype else None
     try:
         fwtyperelease = releases[release]
         fwtypebranch = branches[branch]
     except KeyError as e:
         print(f"Unknown release/branch: {e}")
         sys.exit(2)
-    fwtyperelease['subtypes'] = [fwtypebranch]
+    if clitype:
+        try:
+            fwtypecli = clitypes[clitype]
+            fwtypecli['subtypes'] = [fwtypebranch]
+            fwtyperelease['subtypes'] = [fwtypecli]
+
+        except KeyError as e:
+            print(f"Unknown clitype: {e}")
+            sys.exit(2)
+    else:
+        fwtyperelease['subtypes'] = [fwtypebranch]
     # channel
     if channel in channels:
         fwtypechannel = channels[channel]
